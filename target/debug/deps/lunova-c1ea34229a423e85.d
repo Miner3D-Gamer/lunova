92e30e04b73403504dec2254ac6a7c5d7af9e8e8
Miner3DGaming <105589865+Miner3DGaming@users.noreply.github.com>
2026-02-11 18:06:19 +0100

    Test

--- target/debug/deps/lunova-c1ea34229a423e85.d
+++
@@ -1,9 +0,0 @@
-C:\personal\games\libraries\Rusty\lunova\target\debug\deps\lunova-c1ea34229a423e85.d: lunova\src\main.rs Cargo.toml
-
-C:\personal\games\libraries\Rusty\lunova\target\debug\deps\liblunova-c1ea34229a423e85.rmeta: lunova\src\main.rs Cargo.toml
-
-lunova\src\main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::overly_complex_bool_expr__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
